be3b98fbcfdbd18616e9a24f6584db833eab3357
itsdf07 <923255742@qq.com>
2019-11-22 16:44:56 +0800

    创建的原始项目，尚未运行，直接提交代码

---
+++ src/main/java/com/itsdf07/Itsdf07Application.java
@@ -0,0 +1,13 @@
+package com.itsdf07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Itsdf07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Itsdf07Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/itsdf07/Itsdf07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsdf07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Itsdf07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
