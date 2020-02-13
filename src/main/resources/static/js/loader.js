8b938b66bfc015e664f962298f0b788abeb4e85b
itsdf07 <itsdf07@gmail.com>
2020-02-13 11:24:02 +0800

    提取公共工具

--- src/main/resources/static/js/loader.js
+++ src/main/resources/static/js/loader.js
@@ -5,15 +5,15 @@
  */
 function getRootPath() {
     //如：http://localhost:8087/itsdf07/
-    var curWWWPath = window.document.location.href;
+    let curWWWPath = window.document.location.href;
     //如：/itsdf07/
-    var pathName = window.document.location.pathname;
+    let pathName = window.document.location.pathname;
     //如：21
-    var pos = curWWWPath.indexOf(pathName);
+    let pos = curWWWPath.indexOf(pathName);
     //如：http://localhost:8087
-    var localHostPath = curWWWPath.substring(0, pos);
+    let localHostPath = curWWWPath.substring(0, pos);
     //如：/itsdf07
-    var projectName = pathName.substring(0, pathName.substring(1).indexOf('/') + 1);
+    let projectName = pathName.substring(0, pathName.substring(1).indexOf('/') + 1);
     console.log("curWWWPath:" + curWWWPath);
     console.log("pathName:" + pathName);
     console.log("pos:" + pos);
@@ -28,3 +28,27 @@ document.write('<script src="' + getRootPath() + '/js/jquery-3.3.1.min.js"></scr
 document.write('<script src="' + getRootPath() + '/js/bootstrap.min.js"></script>');
 document.write('<link rel="stylesheet" href="' + getRootPath() + '/css/bootstrap.min.css"/>');
 
+
+/**
+ * 时间格式，如2019-11-22 16-32-04
+ * @param fmt
+ * @param date
+ * @returns {void | string}
+ */
+function dateFtt(fmt, date) {
+    let o = {
+        "M+": date.getMonth() + 1,                 //月份
+        "d+": date.getDate(),                    //日
+        "h+": date.getHours(),                   //小时
+        "m+": date.getMinutes(),                 //分
+        "s+": date.getSeconds(),                 //秒
+        "q+": Math.floor((date.getMonth() + 3) / 3), //季度
+        "S": date.getMilliseconds()             //毫秒
+    };
+    if (/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (let k in o)
+        if (new RegExp("(" + k + ")").test(fmt))
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
